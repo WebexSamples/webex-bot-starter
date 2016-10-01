f07bb50473b8a453f47d1bf85b6ea46993e770b8
Victor Algaze <valgaze@gmail.com>
2016-10-01 14:22:08 -0700

    Initial project scaffolding with empty config file

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # sparkbotstarter
-Starter kit template for a simple Cisco Spark bot 
+Starter kit template for a simple Cisco Spark bot
+
+# Checklist (absolute bare minimum to get a helloworld bot working)
+
+- [ ] Sign up for Cisco Spark
+
+- [ ] Create a Cisco Spark Bot (save the API key)
+
+- [ ] Sign up for nGrok (save API key) and start it on your machine (save the port number and public web address)
+
+- [ ] Join a room in Cisco sparkbotstarter
+
+- [ ] Obtain the roomId from the Cisco Spark API
+
+- [ ] Create a webhook with the roomId and using your nGrok address
+
+- [ ] Add the
+
+- [ ] Turn on your bot server with ```npm start```
+
+//Todo: Add accompanying blog post, easy explanation, Postman instructions, links to other resources and tutorial, Licecap Gifs, example of what the bot will do
--- index.js
+++ index.js
@@ -1,1 +1,77 @@
-console.log("all is firing!")
+/*
+Heavily based off Nick Marcus' node-flint framework helloworld example: https://github.com/nmarus/flint
+*/
+
+var Flint = require('node-flint');
+var webhook = require('node-flint/webhook');
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+app.use(bodyParser.json());
+const config = require("./config.json");
+
+// init flint
+var flint = new Flint(config);
+flint.start();
+console.log("Starting flint, please wait...");
+
+flint.on("initialized", function() {
+  console.log("Flint initialized successfully! [Press CTRL-C to quit]");
+});
+
+/****
+## Process incoming messages
+****/
+
+
+/* On mention with command
+ex User enters @botname /hello, the bot will write back
+*/
+flint.hears('/hello', function(bot, trigger) {
+  console.log("/hello fired");
+  bot.say('%s, you said hello to me!', trigger.personDisplayName);
+});
+
+
+/* On mention with command, using other trigger data, can use lite markdown formatting
+ex "@botname /whoami"
+*/
+flint.hears('/whoami', function(bot, trigger) {
+  console.log("/whoami fired");
+  //the "trigger" parameter gives you access to data about the user who entered the command
+  let roomId = "*" + trigger.roomId + "*";
+  let roomTitle = "**" + trigger.roomTitle + "**";
+  let personEmail = trigger.personEmail;
+  let personDisplayName = trigger.personDisplayName;
+  let outputString = `${personDisplayName} here is some of your information: \n\n\n **Room:** you are in "${roomTitle}" \n\n\n **Room id:** ${roomId} \n\n\n **Email:** your email on file is *${personEmail}*"`;
+  bot.say("markdown", outputString);
+});
+
+/* On mention with command arguments
+ex User enters @botname /echo phrase, the bot will take the arguments and echo them back
+*/
+flint.hears('/echo', function(bot, trigger) {
+  console.log("/echo fired");
+  let phrase = trigger.args.slice(1).join(" ");
+  let outputString = `Ok, I'll say it: "${phrase}"`;
+  bot.say(outputString);
+});
+
+/****
+## Server config & housekeeping
+****/
+
+app.post('/', webhook(flint));
+
+var server = app.listen(config.port, function () {
+  flint.debug('Flint listening on port %s', config.port);
+});
+
+// gracefully shutdown (ctrl-c)
+process.on('SIGINT', function() {
+  flint.debug('stoppping...');
+  server.close();
+  flint.stop().then(function() {
+    process.exit();
+  });
+});
